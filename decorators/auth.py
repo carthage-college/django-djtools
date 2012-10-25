f2ed28bca7d96e1955e2d6ee1b14b7e5d6d9be9a
Bender <plungerman@gmail.com>
2012-10-25 08:23:29 -0500

    decorators collection

---
+++ decorators/auth.py
@@ -0,0 +1,23 @@
+from django.core.exceptions import PermissionDenied
+
+def superuser_only(function):
+    """
+    Limit view to superusers only.
+    Usage:
+    --------------------------------------------------------------------------
+    @superuser_only
+    def my_view(request):
+        ...
+    --------------------------------------------------------------------------
+    or in urls:
+    --------------------------------------------------------------------------
+    urlpatterns = patterns('',
+        (r'^foobar/(.*)', superuser_only(my_view)),
+    )
+    --------------------------------------------------------------------------
+    """
+    def _inner(request, *args, **kwargs):
+        if not request.user.is_superuser:
+            raise PermissionDenied
+        return function(request, *args, **kwargs)
+    return _inner
