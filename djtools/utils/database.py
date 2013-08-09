3982e94793e796e6c4d73c120896e7439f0de3ed
Bender <plungerman@gmail.com>
2013-08-09 07:36:32 -0500

    new database connection method for mysqldb

---
+++ djtools/utils/database.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+
+import MySQLdb
+
+def mysql_db(sql):
+    hs = settings.DATABASES['livewhale']['HOST']
+    us = settings.DATABASES['livewhale']['USER']
+    ps = settings.DATABASES['livewhale']['PASSWORD']
+    db = settings.DATABASES['livewhale']['NAME']
+    conn  = MySQLdb.connect(host=hs,user=us,passwd=ps,db=db)
+    curr = conn.cursor()
+    curr.execute(sql)
+    conn.commit()
+    curr.close ()
+    conn.close ()
