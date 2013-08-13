9cb2069ce708b2a3c7c3fca314e9bd26ffabe4d1
Bender <plungerman@gmail.com>
2013-08-13 10:32:11 -0500

    switch for request object or not

--- djtools/utils/mail.py
+++ djtools/utils/mail.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.template import RequestContext
+from django.template import RequestContext, Context
 from django.template import loader
 from django.http import HttpResponseServerError, HttpResponseNotFound
 from django.shortcuts import render_to_response
@@ -9,7 +9,10 @@ def send_mail(request, recipients, subject, femail, template, data, bcc=None):
     if not bcc:
         bcc = settings.MANAGERS
     t = loader.get_template(template)
-    c = RequestContext(request, {'data':data,})
+    if request:
+        c = RequestContext(request, {'data':data,})
+    else:
+        c = Context({'data':data,})
     headers = {'Reply-To': femail,'From': femail,}
     email = EmailMessage(subject, t.render(c), femail, recipients, bcc, headers=headers)
     email.content_subtype = "html"
