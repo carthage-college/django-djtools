78fa7fe6a508ec79551da6960d37c98742afac71
larry <larry@carthage.edu>
2013-11-21 11:05:57 -0600

    new URLs from settings for use in templates

--- djtools/context_processors.py
+++ djtools/context_processors.py
@@ -7,11 +7,14 @@ def sitevars(request):
     context['site_id'] = current_site.id
     context['site_name'] = current_site.name
     context['site_domain'] = current_site.domain
+    context['static_root'] = settings.MEDIA_ROOT
     context['media_root'] = settings.MEDIA_ROOT
     context['media_url'] = settings.MEDIA_URL
+    context['static_url'] = settings.STATIC_URL
     context['server_url'] = settings.SERVER_URL
     context['root_url'] = settings.ROOT_URL
     context['login_url'] = settings.LOGIN_URL
+    context['logout_url'] = settings.LOGOUT_URL
     context['django_debug'] = settings.DEBUG
     context['template_debug'] = settings.TEMPLATE_DEBUG
     return context
