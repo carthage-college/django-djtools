7c5044b4cccac51e2c3964b217c46bc4b38e6f7e
Bender <plungerman@gmail.com>
2013-11-21 11:13:12 -0600

    try/except for all values, in case they don't exist, only templates are affected, and no 500 error.

--- djtools/context_processors.py
+++ djtools/context_processors.py
@@ -3,18 +3,21 @@ from django.contrib.sites.models import Site
 
 def sitevars(request):
     context = {}
-    current_site = Site.objects.get_current()
-    context['site_id'] = current_site.id
-    context['site_name'] = current_site.name
-    context['site_domain'] = current_site.domain
-    context['static_root'] = settings.MEDIA_ROOT
-    context['media_root'] = settings.MEDIA_ROOT
-    context['media_url'] = settings.MEDIA_URL
-    context['static_url'] = settings.STATIC_URL
-    context['server_url'] = settings.SERVER_URL
-    context['root_url'] = settings.ROOT_URL
-    context['login_url'] = settings.LOGIN_URL
-    context['logout_url'] = settings.LOGOUT_URL
-    context['django_debug'] = settings.DEBUG
-    context['template_debug'] = settings.TEMPLATE_DEBUG
+    try:
+        current_site = Site.objects.get_current()
+        context['site_id'] = current_site.id
+        context['site_name'] = current_site.name
+        context['site_domain'] = current_site.domain
+        context['static_root'] = settings.MEDIA_ROOT
+        context['media_root'] = settings.MEDIA_ROOT
+        context['media_url'] = settings.MEDIA_URL
+        context['static_url'] = settings.STATIC_URL
+        context['server_url'] = settings.SERVER_URL
+        context['root_url'] = settings.ROOT_URL
+        context['login_url'] = settings.LOGIN_URL
+        context['logout_url'] = settings.LOGOUT_URL
+        context['django_debug'] = settings.DEBUG
+        context['template_debug'] = settings.TEMPLATE_DEBUG
+    except:
+        pass
     return context
