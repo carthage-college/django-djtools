163a5f9513104c0639ac3555ae86daba4e95d8e2
Larry <larry@carthage.edu>
2014-02-04 12:34:18 -0600

    moved generic filters and template tags from directory project to here

--- djtools/templatetags/filters.py
+++ djtools/templatetags/filters.py
@@ -10,9 +10,48 @@ register = template.Library()
 
 @register.filter()
 @template.defaultfilters.stringfilter
+def format_phone(value):
+    """
+    takes a value with separated by dashes and returns it in the format:
+    (415) 963-4949
+    """
+    phone = value.split("-")
+    try:
+        phone = "(%s) %s-%s" % (phone[0],phone[1],phone[2])
+    except:
+        if value:
+            phone = "(%s) %s-%s" % (value[:3],value[3:6],value[6:10])
+        else:
+            phone = value
+    return phone
+format_phone.is_safe=True
+format_phone.needs_autoescape = False
+
+@register.filter()
+@template.defaultfilters.stringfilter
+def get_novell_username(value):
+    novell = value.split("@")
+    try:
+        novell = novell[0]
+    except:
+        novell = ''
+    return novell
+
+@register.filter()
+@template.defaultfilters.stringfilter
+def verify_earl(value):
+    try:
+        resp = urllib2.urlopen(value)
+        return True
+    except urllib2.URLError, e:
+        return False
+
+@register.filter()
+@template.defaultfilters.stringfilter
 def encrypt(value):
     try:
         encoded = do_crypt(value, "encrypt").replace("/","\/")
     except:
         encoded = ''
     return encoded
+
