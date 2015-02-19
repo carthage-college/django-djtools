58e441e421d4925b17bf111a5be14e3733817a6c
Bender <plungerman@gmail.com>
2015-02-19 12:21:14 -0600

    new sitevars for email UI assistance

--- djtools/context_processors.py
+++ djtools/context_processors.py
@@ -18,6 +18,22 @@ def sitevars(request):
         context['logout_url'] = settings.LOGOUT_URL
         context['django_debug'] = settings.DEBUG
         context['template_debug'] = settings.TEMPLATE_DEBUG
+        # UI helpers for email
+        context['dl_dt'] = '''
+            style="background:#5f9be3; color:#fff; float:left; font-weight:bold; margin-right:10px; padding:5px; width:200px;"
+        '''
+        context['dl_dd'] = '''
+            style="margin:2px 0; padding:5px 0;"
+        '''
+        context['dl_detail'] = '''
+            style="margin-bottom:5px;"
+        '''
+        context['dd_desc'] = '''
+            style="margin-bottom:7px 0;"
+        '''
+        context['clear'] = '''
+            style="clear:both;"
+        '''
     except:
         pass
     return context
