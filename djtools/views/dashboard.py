9d8f40d583596fda8249a027120a2399a31990c5
Bender <plungerman@gmail.com>
2015-08-17 13:06:21 -0500

    moved responsive switch for dashboard (djdash) UI from various projects using it to here

---
+++ djtools/views/dashboard.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponseRedirect
+
+def responsive_switch(request,action):
+    if action=="go":
+        request.session['desktop_mode']=True
+    elif action=="leave":
+        request.session['desktop_mode']=False
+    return HttpResponseRedirect(request.META.get("HTTP_REFERER", ""))
