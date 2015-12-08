c829b0bcbe02d251ea63e83e4b4bcb2a35e0a22c
Bender <plungerman@gmail.com>
2015-12-08 13:35:14 -0600

    new decorator for checking credentials with portal UID and django groups

--- djtools/decorators/auth.py
+++ djtools/decorators/auth.py
@@ -1,7 +1,41 @@
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse_lazy
 from django.core.exceptions import PermissionDenied
 from django.contrib.auth.decorators import user_passes_test
+from django.utils.decorators import available_attrs
+from django.http import HttpResponseRedirect
+from django.utils.encoding import force_str
+from django.shortcuts import resolve_url
+
+from djzbar.utils.mssql import get_userid
+from djtools.utils.users import in_group
+
+from functools import wraps
+
+def portal_auth_required(redirect_url=None):
+    """
+    Restrict access to a view that is embedded within an iframe
+    in the portal
+    """
+
+    def decorator(view_func):
+        @wraps(view_func, assigned=available_attrs(view_func))
+        def _wrapped_view(request, *args, **kwargs):
+            response = view_func(request, *args, **kwargs)
+            resolved_redirect_url = force_str(
+                resolve_url(redirect_url or reverse_lazy("access_denied"))
+            )
+            uid = get_userid(request.GET.get('uid'))
+            user = User.objects.get(pk=uid)
+            if not in_group(user, "BusinessOfficeAdmin") \
+                or not user.is_superuser:
+                #return HttpResponseRedirect(resolved_redirect_url)
+                return view_func(request, *args, **kwargs)
+            return view_func(request, *args, **kwargs)
+        return _wrapped_view
+    return decorator
+
 
 def superuser_only(function):
     """
