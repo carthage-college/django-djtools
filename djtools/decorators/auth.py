7cc622e0f9766bbe26f24db76da486858948773f
Bender <plungerman@gmail.com>
2015-12-09 12:41:55 -0600

    added group name to portal_auth_required() decorator

--- djtools/decorators/auth.py
+++ djtools/decorators/auth.py
@@ -13,7 +13,7 @@ from djtools.utils.users import in_group
 
 from functools import wraps
 
-def portal_auth_required(redirect_url=None):
+def portal_auth_required(group=None, redirect_url=None):
     """
     Restrict access to a view that is embedded within an iframe
     in the portal
@@ -31,9 +31,9 @@ def portal_auth_required(redirect_url=None):
                 user = User.objects.get(pk=uid)
             except:
                 return HttpResponseRedirect(resolved_redirect_url)
-            if not in_group(user, "BusinessOfficeAdmin") and not user.is_superuser:
-                #return view_func(request, *args, **kwargs)
-                return HttpResponseRedirect(resolved_redirect_url)
+            if group:
+                if not in_group(user, group) and not user.is_superuser:
+                    return HttpResponseRedirect(resolved_redirect_url)
             return view_func(request, *args, **kwargs)
         return _wrapped_view
     return decorator
