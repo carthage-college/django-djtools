6922a324f9f975759f2bb628753b36662287dfed
Bender <plungerman@gmail.com>
2016-01-24 18:31:55 -0600

    portal_auth_required decorator now allows you to sign in from portal or from django app itself

--- djtools/decorators/auth.py
+++ djtools/decorators/auth.py
@@ -6,17 +6,17 @@ from django.contrib.auth.decorators import user_passes_test
 from django.utils.decorators import available_attrs
 from django.http import HttpResponseRedirect
 from django.utils.encoding import force_str
+from django.utils.functional import wraps
 from django.shortcuts import resolve_url
+from django.contrib.auth import login
 
 from djzbar.utils.mssql import get_userid
 from djtools.utils.users import in_group
 
-from functools import wraps
 
 def portal_auth_required(group=None, redirect_url=None):
     """
-    Restrict access to a view that is embedded within an iframe
-    in the portal
+    Restrict access to a view that receives UID from the portal
     """
 
     def decorator(view_func):
@@ -26,14 +26,24 @@ def portal_auth_required(group=None, redirect_url=None):
             resolved_redirect_url = force_str(
                 resolve_url(redirect_url or reverse_lazy("access_denied"))
             )
-            uid = get_userid(request.GET.get('uid'))
-            try:
-                user = User.objects.get(pk=uid)
-            except:
-                return HttpResponseRedirect(resolved_redirect_url)
-            if group:
-                if not in_group(user, group) and not user.is_superuser:
-                    return HttpResponseRedirect(resolved_redirect_url)
+            if not request.session.get("BusinessOfficeFinance"):
+                if not request.user.is_authenticated():
+                    # coming from the portal with uid set?
+                    uid = get_userid(request.GET.get('uid'))
+                    try:
+                        user = User.objects.get(pk=uid)
+                    except:
+                        # nope
+                        return HttpResponseRedirect(resolved_redirect_url)
+                else:
+                    user = request.user
+                if group:
+                    if not in_group(user, group) and not user.is_superuser:
+                        return HttpResponseRedirect(resolved_redirect_url)
+                # sign in the user manually
+                user.backend = 'django.contrib.auth.backends.ModelBackend'
+                login(request, user)
+                request.session["BusinessOfficeFinance"] = True
             return view_func(request, *args, **kwargs)
         return _wrapped_view
     return decorator
