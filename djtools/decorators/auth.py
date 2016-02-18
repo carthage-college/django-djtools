c2c26c8fec4b5c54c6e572d4422b27e5c0b35ef2
Bender <plungerman@gmail.com>
2016-02-18 12:05:55 -0600

    fixed portal required decorator but not certain how; i think the use of 'decorator' in the name of one of the functions might have been problematic. not certain but now it works without sending a POST request twice

--- djtools/decorators/auth.py
+++ djtools/decorators/auth.py
@@ -6,34 +6,36 @@ from django.contrib.auth.decorators import user_passes_test
 from django.utils.decorators import available_attrs
 from django.http import HttpResponseRedirect
 from django.utils.encoding import force_str
-from django.utils.functional import wraps
+#from django.utils.functional import wraps
 from django.shortcuts import resolve_url
 from django.contrib.auth import login
 
 from djzbar.utils.mssql import get_userid
 from djtools.utils.users import in_group
 
+from functools import wraps
 
-def portal_auth_required(group=None, redirect_url=None):
-    """
-    Restrict access to a view that receives UID from the portal
-    """
-
-    def decorator(view_func):
+def portal_auth_required(session_var, group=None, redirect_url=None):
+    def _portal_auth_required(view_func):
         @wraps(view_func, assigned=available_attrs(view_func))
-        def _wrapped_view(request, *args, **kwargs):
-            response = view_func(request, *args, **kwargs)
+        def wrapper(request, *args, **kwargs):
             resolved_redirect_url = force_str(
                 resolve_url(redirect_url or reverse_lazy("auth_login"))
             )
-            if not request.session.get("BusinessOfficeFinance"):
+            if not request.session.get(session_var):
                 if not request.user.is_authenticated():
                     # coming from the portal with uid set?
-                    uid = get_userid(request.GET.get('uid'))
-                    try:
-                        user = User.objects.get(pk=uid)
-                    except:
-                        # nope
+                    uid = request.GET.get('uid')
+                    if uid:
+                        # do we have a user id or portal id?
+                        if len(uid) > 12:
+                            uid = get_userid(request.GET.get('uid'))
+                        try:
+                            user = User.objects.get(pk=uid.strip("0"))
+                        except:
+                            # nope
+                            return HttpResponseRedirect(reverse_lazy("auth_login"))
+                    else:
                         return HttpResponseRedirect(reverse_lazy("auth_login"))
                 else:
                     user = request.user
@@ -43,10 +45,11 @@ def portal_auth_required(group=None, redirect_url=None):
                 # sign in the user manually
                 user.backend = 'django.contrib.auth.backends.ModelBackend'
                 login(request, user)
-                request.session["BusinessOfficeFinance"] = True
+                request.session[session_var] = True
+
             return view_func(request, *args, **kwargs)
-        return _wrapped_view
-    return decorator
+        return wrapper
+    return _portal_auth_required
 
 
 def superuser_only(function):
