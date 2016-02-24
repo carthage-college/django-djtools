42340208b25a77d16f40370659748dff59094009
Bender <plungerman@gmail.com>
2016-02-24 12:01:56 -0600

    moved GroupCheckNode() class above the ifusergroup() function

--- djtools/templatetags/ifusergroup.py
+++ djtools/templatetags/ifusergroup.py
@@ -4,6 +4,34 @@ from django.contrib.auth.models import Group
 
 register = template.Library()
 
+class GroupCheckNode(template.Node):
+    def __init__(self, groups, nodelist_true, nodelist_false):
+        self.groups = groups
+        self.nodelist_true = nodelist_true
+        self.nodelist_false = nodelist_false
+    def render(self, context):
+        user = resolve_variable('user', context)
+
+        if not user.is_authenticated():
+            return self.nodelist_false.render(context)
+
+        allowed=False
+        for checkgroup in self.groups:
+            try:
+                group = Group.objects.get(name=checkgroup)
+            except Group.DoesNotExist:
+                break
+
+            if group in user.groups.all():
+                allowed=True
+                break
+
+        if allowed:
+            return self.nodelist_true.render(context)
+        else:
+            return self.nodelist_false.render(context)
+
+
 @register.tag()
 def ifusergroup(parser, token):
     """
@@ -36,31 +64,3 @@ def ifusergroup(parser, token):
         nodelist_false = NodeList()
 
     return GroupCheckNode(groups, nodelist_true, nodelist_false)
-
-
-class GroupCheckNode(template.Node):
-    def __init__(self, groups, nodelist_true, nodelist_false):
-        self.groups = groups
-        self.nodelist_true = nodelist_true
-        self.nodelist_false = nodelist_false
-    def render(self, context):
-        user = resolve_variable('user', context)
-
-        if not user.is_authenticated():
-            return self.nodelist_false.render(context)
-
-        allowed=False
-        for checkgroup in self.groups:
-            try:
-                group = Group.objects.get(name=checkgroup)
-            except Group.DoesNotExist:
-                break
-
-            if group in user.groups.all():
-                allowed=True
-                break
-
-        if allowed:
-            return self.nodelist_true.render(context)
-        else:
-            return self.nodelist_false.render(context)
