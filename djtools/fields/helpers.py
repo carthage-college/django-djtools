ae0710403e16612c0e47726bc5b240d0e47b0a8a
Bender <plungerman@gmail.com>
2016-03-20 11:45:27 -0500

    new function to handle uploaded files by renaming the file to a random string, combine it with a file system 'path', and write to new desitination

--- djtools/fields/helpers.py
+++ djtools/fields/helpers.py
@@ -13,3 +13,22 @@ def upload_to_path(self, filename):
         self.get_slug()
     )
     return join(path, filename)
+
+
+def handle_uploaded_file(f, path):
+    """
+    Rename file to random string, combine it with 'path'
+    and write to new destination. Handy when not using
+    a model class for your form.
+    """
+    # obtain the file extension
+    ext = f.name.split(".")[-1]
+    # set filename as random string
+    filename = '{}.{}'.format(uuid4().hex, ext)
+    # combine it all together
+    phile = join(path, filename)
+    # and write
+    with open(phile, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+    return filename
