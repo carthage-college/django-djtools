58c2b11bd84027ad4dfb0053fe7bf843a9bb94a6
plungerman <plungerman@gmail.com>
2016-08-21 11:37:16 -0500

    added some debugging for files that are being difficult

--- djtools/fields/validators.py
+++ djtools/fields/validators.py
@@ -3,15 +3,18 @@ from django.core.exceptions import ValidationError
 
 import magic
 
+
 class MimetypeValidator(object):
     def __init__(self, mimetypes):
         self.mimetypes = mimetypes
 
     def __call__(self, value):
         try:
-            mime = magic.from_buffer(value.read(1024), mime=True)
+            mime = magic.from_buffer(value.read(), mime=True)
             if not mime in self.mimetypes:
                 raise ValidationError('%s is not an acceptable file type' % value)
+                #raise ValidationError('%s is not an acceptable file type' % mime)
+                #raise ValidationError('{} {}'.format(value, mime))
         except AttributeError as e:
             raise ValidationError('This value could not be validated for file type' % value)
 
