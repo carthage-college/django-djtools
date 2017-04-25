3bcf6126270983b116b8b4e326f3cc2cfd0d6536
plungerman <plungerman@gmail.com>
2017-04-25 10:09:39 -0500

    updated error views to work with  django1.6 and above to django 1.11

--- djtools/views/errors.py
+++ djtools/views/errors.py
@@ -2,6 +2,9 @@ from django.conf import settings
 from django.template import loader, Context
 from django.http import HttpResponseServerError, HttpResponseNotFound
 
+import django
+
+
 def server_error(request, template_name='500.html'):
     """
     500 error handler.    Templates: `500.html`
@@ -12,7 +15,16 @@ def server_error(request, template_name='500.html'):
 
     t = loader.get_template(template_name)
 
-    rendered = t.render({'media_url': settings.MEDIA_URL,'layout':[0,1]}, request)
+    # VERSION returns (1, x, x, u'final', 1)
+    # hopefully, we will be done using django 1.6 by the time 2.x comes out
+    if django.VERSION[1] > 6:
+        rendered = t.render(
+            {'media_url': settings.MEDIA_URL,'layout':[0,1]}, request
+        )
+    else:
+        rendered = t.render(Context({
+            'media_url': settings.MEDIA_URL,'layout':[0,1],
+        }))
 
     return HttpResponseNotFound(rendered)
 
@@ -29,6 +41,15 @@ def four_oh_four_error(request, template_name='404.html'):
 
     t = loader.get_template(template_name)
 
-    rendered = t.render({'media_url': settings.MEDIA_URL,'layout':[0,1]}, request)
+    # VERSION returns (1, x, x, u'final', 1)
+    # hopefully, we will be done using django 1.6 by the time 2.x comes out
+    if django.VERSION[1] > 6:
+        rendered = t.render(
+            {'media_url': settings.MEDIA_URL,'layout':[0,1]}, request
+        )
+    else:
+        rendered = t.render(Context({
+            'media_url': settings.MEDIA_URL,'layout':[0,1],
+        }))
 
     return HttpResponseNotFound(rendered)
