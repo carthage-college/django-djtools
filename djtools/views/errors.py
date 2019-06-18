3f6cb17bf4f3d2b85a59415deeb9c509873ea8fc
plungerman <plungerman@gmail.com>
2019-06-18 16:40:35 -0500

    updated error page views to remove old code that has not worked since django 1.5

--- djtools/views/errors.py
+++ djtools/views/errors.py
@@ -1,30 +1,19 @@
 from django.conf import settings
-from django.template import loader, Context
+from django.template import loader
 from django.http import HttpResponseServerError, HttpResponseNotFound
-
-import django
 
 
 def server_error(request, template_name='500.html'):
     """
-    500 error handler.    Templates: `500.html`
-    Context:
-        media_url
-            Path of static media (e.g. "media.example.org")
+    500 error handler.
+    Template: `500.html`
     """
 
     t = loader.get_template(template_name)
 
-    # VERSION returns (1, x, x, u'final', 1)
-    # hopefully, we will be done using django 1.6 by the time 2.x comes out
-    if django.VERSION[1] > 6:
-        rendered = t.render(
-            {'media_url': settings.MEDIA_URL,'layout':[0,1]}, request
-        )
-    else:
-        rendered = t.render(Context({
-            'media_url': settings.MEDIA_URL,'layout':[0,1],
-        }))
+    rendered = t.render(
+        {'media_url': settings.MEDIA_URL,'layout':[0,1]}, request
+    )
 
     return HttpResponseNotFound(rendered)
 
@@ -32,24 +21,13 @@ def server_error(request, template_name='500.html'):
 def four_oh_four_error(request, template_name='404.html'):
     """
     404 error handler.
-
-    Templates: `404.html`
-    Context:
-        media_url
-            Path of static media (e.g. "media.example.org")
+    Template: `404.html`
     """
 
     t = loader.get_template(template_name)
 
-    # VERSION returns (1, x, x, u'final', 1)
-    # hopefully, we will be done using django 1.6 by the time 2.x comes out
-    if django.VERSION[1] > 6:
-        rendered = t.render(
-            {'media_url': settings.MEDIA_URL,'layout':[0,1]}, request
-        )
-    else:
-        rendered = t.render(Context({
-            'media_url': settings.MEDIA_URL,'layout':[0,1],
-        }))
+    rendered = t.render(
+        {'media_url': settings.MEDIA_URL,'layout':[0,1]}, request
+    )
 
     return HttpResponseNotFound(rendered)
