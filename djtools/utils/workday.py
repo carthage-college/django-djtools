799c3ad5c35633d7ed256477821be1a471de0fe5
plungerman <plungerman@gmail.com>
2023-04-13 09:59:16 -0500

    updates to rest api for workday data

--- djtools/utils/workday.py
+++ djtools/utils/workday.py
@@ -5,6 +5,9 @@ from django.conf import settings
 from django.core.cache import cache
 
 
+HEADERS = {'Authorization': 'Token {0}'.format(settings.REST_FRAMEWORK_TOKEN)}
+
+
 def department_detail(did):
     """Fetch a department from the API based on department ID."""
     dept = []
@@ -13,7 +16,7 @@ def department_detail(did):
             settings.DIRECTORY_API_URL,
             did,
         ),
-        headers={'Authorization': settings.REST_FRAMEWORK_TOKEN},
+        headers=HEADERS,
     )
     if response.json():
         dept = response.json()[0]
@@ -30,7 +33,7 @@ def department_all(choices=False):
         '{0}department/?format=json'.format(
             settings.DIRECTORY_API_URL,
         ),
-        headers={'Authorization': settings.REST_FRAMEWORK_TOKEN},
+        headers=HEADERS,
     )
     if response.json():
         for dept in response.json():
@@ -52,7 +55,7 @@ def department_person(cid, choices=False):
             settings.DIRECTORY_API_URL,
             cid,
         ),
-        headers={'Authorization': settings.REST_FRAMEWORK_TOKEN},
+        headers=HEADERS,
     )
     if response.json():
         for dept in response.json()[0]['departments']:
@@ -63,6 +66,40 @@ def department_person(cid, choices=False):
                 else:
                     depts.append(department)
     return depts
+
+
+def get_deans(cid=False):
+    """Obtain all deans or a dean."""
+    deans = []
+    response = requests.get(
+        '{0}profile/deans/?format=json'.format(
+            settings.DIRECTORY_API_URL,
+        ),
+        headers=HEADERS,
+    )
+    if response.json():
+        for dean in response.json():
+            if cid and cid == dean['id']:
+                return response.json()
+            else:
+                deans.append(dean)
+    return deans
+
+
+def get_peep(cid):
+    """Obtain the profile based on ID."""
+    key = 'workday_{0}_api'.format(cid)
+    peep = cache.get(key)
+    if not peep:
+        earl = '{0}profile/{1}/detail/?format=json'.format(
+            settings.DIRECTORY_API_URL,
+            cid,
+        )
+        response = requests.get(earl, headers=HEADERS)
+        if response.json():
+            peep = response.json()
+            cache.set(key, peep, timeout=86400)
+    return peep
 
 
 def get_peeps(who=None, choices=False):
@@ -82,18 +119,14 @@ def get_peeps(who=None, choices=False):
             earl = '{0}profile/?format=json'.format(
                 settings.DIRECTORY_API_URL,
             )
-        response = requests.get(
-            earl,
-            headers={'Authorization': settings.REST_FRAMEWORK_TOKEN},
-        )
+        response = requests.get(earl, headers=HEADERS)
         if response.json():
             for peep in response.json():
                 name = '{0}, {1}'.format(peep['last_name'], peep['first_name'])
                 if choices:
-                    peeps.append((peep.id, name))
+                    peeps.append((peep['id'], name))
                 else:
                     peeps.append(peep)
-                peeps.append(row)
             cache.set(key, peeps, timeout=86400)
 
     return peeps
