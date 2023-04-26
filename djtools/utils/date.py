03fcc7565e44430bfb5362289d525fcd13550039
plungerman <plungerman@gmail.com>
2023-04-26 10:30:00 -0500

    moved get_term() from djsani to here

--- djtools/utils/date.py
+++ djtools/utils/date.py
@@ -1,7 +1,26 @@
-from datetime import date
+# -*- coding: utf-8 -*-
+
+import datetime
+from django.conf import settings
+
+
+def get_term(start_date=None):
+    """Obtain the current academic term."""
+    if not start_date:
+        start_date = settings.START_DATE
+    today = datetime.date.today()
+    term = 'RA'
+    year = today.year
+    if ((today.month < start_date.month) or (today.month == 12 and today.day > 10)):
+        term = 'RC'
+        if today.month == 12:
+            year = year + 1
+    return {'yr': year, 'sess': term}
+
+
 
 def calculate_age(born):
-    today = date.today()
+    today = datetime.date.today()
     return today.year - born.year - (
         (today.month, today.day) < (born.month, born.day)
     )
