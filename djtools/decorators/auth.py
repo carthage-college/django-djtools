c81fea318ffccf39dd92f2e7762173ef71c07fe8
plungerman <plungerman@gmail.com>
2023-05-23 13:22:57 -0500

    removed super user exception for group_required() decorator

--- djtools/decorators/auth.py
+++ djtools/decorators/auth.py
@@ -35,7 +35,7 @@ def group_required(*group_names):
 
     """
     def in_groups(u):
-        if u.is_superuser or bool(u.groups.filter(name__in=group_names)):
+        if bool(u.groups.filter(name__in=group_names)):
             return True
         return False
     return user_passes_test(in_groups)
